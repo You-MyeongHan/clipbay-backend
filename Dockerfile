f7f16ba25ed20998c63cc2041f7433f3e9e0ab06
home computer <mhy5413@naver.com>
2023-06-24 22:23:18 +0900

    modify test folder and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
-FROM gradle:7.6-jdk17-ubuntu:latest as builder
-ARG JAR_FILE=build/libs/homepage-0.0.1-SNAPSHOT.jar
+FROM gradle:7.6-jdk17-alpine as builder
+
+COPY build.gradle settings.gradle .
+ARG JAR_FILE=./build/libs/homepage-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
 ENTRYPOINT ["java", "-jar", "/app.jar"]
+docker build -t 
--- src/main/java/com/homepage/test/TestController.java
+++ src/main/java/com/homepage/test/TestController.java
@@ -1,11 +1,13 @@
-package com.homepage.security.test;
+package com.homepage.test;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/test")
 public class TestController {
-	@RequestMapping("/api/test")
+	@GetMapping("/")
 	public String test() {
 		return "test checkpoint";
 	}
